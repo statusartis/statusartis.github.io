ddb5d664edc9bb72dca08f0fd20325284332e976
status-artis-deployment <deploy+statusartis@users.noreply.github.com>
2022-03-11 17:39:01 +0000

    deploy: statusartis/statusartis@71174fe3bae10b267833e8d536eede92277196f3

--- js/custom.js
+++ js/custom.js
@@ -7,7 +7,7 @@
   for (const dropdown of dropdowns) {
     dropdown.addEventListener("mouseenter", () => {
       const detailsElements = dropdown.getElementsByTagName("details");
-      if (!detailsElements[0] || window.innerWidth < 768) {
+      if (!detailsElements[0] || window.innerWidth < 992) {
         return;
       }
       detailsElements[0].setAttribute("open", "");
@@ -16,11 +16,26 @@
 
     dropdown.addEventListener("mouseleave", () => {
       const detailsElements = dropdown.getElementsByTagName("details");
-      if (!detailsElements[0] || window.innerWidth < 768) {
+      if (!detailsElements[0] || window.innerWidth < 992) {
         return;
       }
       detailsElements[0].removeAttribute("open");
       // console.log("closed");
+    });
+  }
+})();
+
+(async function () {
+  // init gallery/ies
+  const galleries = document.querySelectorAll(".my-gallery");
+  for (const gallery of galleries) {
+    window.lightGallery(gallery, {
+      // plugins: [..., lgThumbnail, ...] will add thumbnails
+      // but the current implementation downloads fallback images as thumbs, worsens lighthouse scores
+      plugins: [lgFullscreen, lgZoom],
+      getCaptionFromTitleOrAlt: false, // do not show alt text as captions when viewing picture in gallery
+      download: false, // disable download button
+      // speed: 200,
     });
   }
 })();
